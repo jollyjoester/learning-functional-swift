87cf5654dfafba543e50838a834b766a226092b9
NanashimaHideyuki <nanashimahideyuki@NanashiyukisMBP.lan>
2017-04-10 01:16:16 +0900

    p19

--- learning-functional-swift.playground/Contents.swift
+++ learning-functional-swift.playground/Contents.swift
@@ -12,6 +12,14 @@ extension Position {
     func inRange(range: Distance) -> Bool {
         return sqrt(x * x + y * y) <= range
     }
+    
+    func minus(p: Position) -> Position {
+        return Position(x: x - p.x, y: y - p.y)
+    }
+    
+    var length: Double {
+        return sqrt(x * x + y * y)
+    }
 }
 
 struct Ship {
@@ -21,6 +29,15 @@ struct Ship {
 }
 
 extension Ship {
+    
+    func EngageShip1(target: Ship, friendly: Ship) -> Bool {
+        let dx = target.position.x - position.x
+        let dy = target.position.y - position.y
+        let targetDistance = sqrt(dx * dx + dy * dy)
+        
+        return targetDistance <= firingRange
+    }
+
     func canSafelyEngageShip1(target: Ship, friendly: Ship) -> Bool {
         let dx = target.position.x - position.x
         let dy = target.position.y - position.y
@@ -31,5 +48,12 @@ extension Ship {
         let friendlyDistance = sqrt(friendlyDx * friendlyDx + friendlyDy * friendlyDy)
         
         return targetDistance <= firingRange && targetDistance > unsafeRange && (friendlyDistance > unsafeRange)
+    }
+    
+    func canSafelyEngageShip2(target: Ship, friendly: Ship) -> Bool {
+        let targetDistance = target.position.minus(p: position).length
+        let friendlyDistance = friendly.position.minus(p: target.position).length
+        return targetDistance <= firingRange && targetDistance > unsafeRange && (friendlyDistance > unsafeRange)
+    
     }
 }
